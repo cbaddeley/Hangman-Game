2c82a50f20ea2d6592b93908e112c338d8653b6a
cbaddeley <corybaddeley@gmail.com>
2017-05-31 22:14:36 -0400

    fixed some for loops and javascript functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
-var wins = 0;
-var losses = 0;
-var wordBank = ['jaguar', 'python', 'gorilla', 'elephant'];
-var currentWord;
-var guesses = [];
-var selectedWord = [];
-var wrongGuess;
+var wins = 0; //This variable tracks the wins
+var losses = 0; //This variable tracks the losses
+var wordBank = ['jaguar', 'python', 'gorilla', 'elephant']; //These are the words that can be selected. The array will lose an element during the selectWord function.
+var currentWord; // This is the word selected by the computer during the selectWord function.
+var guesses = [""]; // Holds the chars the user inputs. I put an empty string in here so I wouldn't need a <= in a later for loop
+var selectedWord = []; //Array of chars from the currentWord variable.
+var wrongGuess = 10; // Setting it at 10 because I want to show it on the page and subtract then rewrite to the DOM.
+var guess = ''; //Determined through a key press. Had to make it global in order to use a function to manipulate the DOM in the "guesses" section.
+var correctCount = 0; //Everytime a guess is appended to the selectedWord elements, this counter goes up in order to check win condition.
 
 function selectWord() { 
 	var pickedWord = Math.floor(Math.random() * wordBank.length); //Math.random generates a number between 0 and 1 and it's multiplied by the array length for available words then it's rounded down.
@@ -24,14 +26,48 @@ function makeTiles() {
 
 document.onkeyup = function(event) {
 	var guess = event.key;
-	for (var i = 0; i < selectedWord.length; i++) {
-		if (guess == selectedWord[i]) {
-			var node = document.createTextNode(selectedWord[i]);
-			var element = document.getElementById("c" + i);
-			element.appendChild(node);
-		}
+	var guessBool = false;
+	var found = false;
+
+	for (var i = 0; i < guesses.length; i++) {
+		if (guess == guesses[i]) {
+			guessBool = true;
+			break;
+		} 
 	}
-};
+	if (!guessBool) {
+		guesses.push(guess);
+		for (var j = 0; j < selectedWord.length; j++) {
+			if (guess == selectedWord[j]) {
+				var node = document.createTextNode(selectedWord[j]);
+				var element = document.getElementById("c" + j);
+				element.appendChild(node)
+				found = true;
+				correctCount++;
+			} 
+		}
+		if (!found) {
+			wrongGuess--;
+		} 
+	}
+	//if (!found && !guessBool) {
+		//Here I need to execute an function that pushes the guess to the "guesses" section of the html and increase wrong guesses in the HTML
+	//}	
+
+	if (wrongGuess == 0) {
+		//Need to write a lose condition
+	}
+
+	if (correctCount == selectedWord.length && wordBank.length > 0) {
+		//Run Win function
+	}
+
+	if (correctCount == selectedWord.length && wordBank.length == 0) {
+		//Run Finished function
+	}
+
+}
+
 
 makeTiles();
 
